ee314c263f7db56cb7c540e7e2e71f3dcb118089
Suyog Soti <suyog.soti@gmail.com>
2022-12-22 09:08:22 -0800

    make wt script smarter

--- wt.py
+++ wt.py
@@ -16,18 +16,15 @@ def main():
     if len(sys.argv) < 2:
         raise(RuntimeError("need a path"))
     path = sys.argv[1]
-    if not os.path.exists(path):
-        if path.find("/") >= 0:
-            raise(RuntimeError("path not exist"))
-        is_in_worktree_cmd = "git rev-parse --is-inside-work-tree".split()
+    is_in_worktree_cmd = "git rev-parse --is-inside-work-tree".split()
+    is_in_worktree = subprocess.run(is_in_worktree_cmd, capture_output=True, encoding="utf-8")
+    while is_in_worktree.stdout.strip() == "true" and not os.path.exists(path):
+        os.chdir("./..")
         is_in_worktree = subprocess.run(is_in_worktree_cmd, capture_output=True, encoding="utf-8")
-        while is_in_worktree.stdout.strip() == "true":
-            os.chdir("./..")
-            is_in_worktree = subprocess.run(is_in_worktree_cmd, capture_output=True, encoding="utf-8")
-        worktree_path = os.path.join(os.curdir, path)
-        if not os.path.exists(worktree_path):
-            subprocess.call(["git", "worktree", "add", path])
-    path = os.path.abspath(path)
+    worktree_path = os.path.abspath(os.path.join(os.curdir, path))
+    if not os.path.exists(worktree_path):
+        subprocess.call(["git", "worktree", "add", path])
+    path = worktree_path
     name = "_".join(path.split("/")[-2:])
     session_name = name.replace(".git", "")
     server = libtmux.Server()
