a7e4f8f4f85001758bc95390ef99ac714c3c61d7
Suyog Soti <suyog.soti@gmail.com>
2023-01-03 22:49:01 -0800

    forgot to add autocmds.lua

---
+++ lua/autocmds.lua
@@ -0,0 +1,23 @@
+-- I don't want to cycle through the quickfix list when I cycle buffers.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern="qf",
+  callback=function ()
+    vim.bo.buflisted = false
+  end,
+  group=vim.api.nvim_create_augroup("QuickfixNoBuflisted", {clear = true}),
+});
+
+-- I want cursorline only on the window I am on
+local cursorline=vim.api.nvim_create_augroup("Cursorline", {clear = true})
+vim.api.nvim_create_autocmd({"VimEnter", "WinEnter"}, {
+  callback =function ()
+    vim.wo.cursorline = true
+  end,
+  group=cursorline,
+})
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback =function ()
+    vim.wo.cursorline = false
+  end,
+  group=cursorline,
+})
