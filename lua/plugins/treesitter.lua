19354e11deed62cef0db6b1f075d35cd9873020a
Suyog Soti <suyog.soti@databricks.com>
2025-04-20 23:45:10 -0700

    fix tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,64 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-    }
+  "nvim-treesitter/nvim-treesitter",
+  version = false, -- last release is way too old and doesn't work on Windows
+  build = ":TSUpdate",
+  event = { "BufEnter" },
+  lazy = false,
+  init = function(plugin)
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  opts_extend = { "ensure_installed" },
+  opts = {
+    auto_install = true,
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "html",
+      "javascript",
+      "json",
+      "jsonnet",
+      "lua",
+      "markdown",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "typescript",
+      "xml",
+      "scala",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]]"] = "@function.outer", --[[ ["}}"] = "@class.outer" ]] },
+        goto_previous_start = { ["[["] = "@function.outer", --[[ ["{{"] = "@class.outer" ]] },
+      },
+    },
+  },
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  ---@param opts TSConfig
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
